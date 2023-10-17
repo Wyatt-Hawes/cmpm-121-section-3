676630eb09853f6a427a83c43a27e65beb3cd3d3
Wyatt <jhawes@ucsc.edu>
2023-10-16 17:52:41 -0700

    end of solo development

--- src/scenes/Play.ts
+++ src/scenes/Play.ts
@@ -10,7 +10,19 @@ export default class Play extends Phaser.Scene {
   starfield?: Phaser.GameObjects.TileSprite;
   spinner?: Phaser.GameObjects.Shape;
 
+  //Spinner Traits
   rotationSpeed = Phaser.Math.PI2 / 1000; // radians per millisecond
+  leftRightVelocity = 0.25;
+  verticalVelocity = 0.25;
+
+  defaultSpinnerX = 350;
+  defaultSpinnerY = 450;
+  defaultAngle = 0;
+  fired = false;
+
+  //Enemy Traits
+  enemies:Phaser.GameObjects.Rectangle[]  = [];
+  enemyVelocity = 0.25;
 
   constructor() {
     super("play");
@@ -41,7 +53,21 @@ export default class Play extends Phaser.Scene {
       )
       .setOrigin(0, 0);
 
-    this.spinner = this.add.rectangle(100, 100, 50, 50, 0xff0000);
+    this.spinner = this.add.rectangle(this.defaultSpinnerX, this.defaultSpinnerY, 10, 10, 0xfc46aa);
+    this.spawnEnemy();
+    
+  }
+
+  resetSpinner() {
+    this.spinner!.x = this.defaultSpinnerX;
+    this.spinner!.y = this.defaultSpinnerY;
+    this.fired = false;
+    this.spinner!.rotation = this.defaultAngle;
+  }
+
+  spawnEnemy() {
+    this.enemies.push(this.add.rectangle(900, Math.random() * 400, 50, 50, 0xFF0000));
+    setTimeout(()=>{this.spawnEnemy()}, Math.random()* 2000 + 500)
   }
 
   update(_timeMs: number, delta: number) {
@@ -49,9 +75,11 @@ export default class Play extends Phaser.Scene {
 
     if (this.left!.isDown) {
       this.spinner!.rotation -= delta * this.rotationSpeed;
+      this.spinner!.x -= delta * this.leftRightVelocity;
     }
     if (this.right!.isDown) {
       this.spinner!.rotation += delta * this.rotationSpeed;
+      this.spinner!.x += delta * this.leftRightVelocity;
     }
 
     if (this.fire!.isDown) {
@@ -61,6 +89,16 @@ export default class Play extends Phaser.Scene {
         duration: 300,
         ease: Phaser.Math.Easing.Sine.Out,
       });
+      this.fired = true;
     }
+    if (this.fired) {
+      this.spinner!.y -= delta * this.verticalVelocity;
+    }
+    if (this.spinner!.y < -10) {
+      this.resetSpinner();
+    }
+    this.enemies.forEach((enemy) => {
+      enemy.x -= delta * this.enemyVelocity;
+    })
   }
 }
